68db185def28e458d543cd2251641e07817fc3e8
Christian KULA <christian.kula.ck@gmail.com>
2017-08-07 17:09:08 +0200

    Created script's entry point

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+import cli.Cli
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.ParameterException
+import manga.Source
+
+val cli = Cli()
+
+val jCommander = JCommander.newBuilder()
+        .addObject(cli)
+        .programName("java -jar vulpes.jar")
+        .build()!!
+
+fun main(args: Array<String>) {
+    try {
+        jCommander.parse(*args)
+    } catch (e: ParameterException) {
+        System.err.println("Incorrect parameters. Please use -h option to show usage")
+        return
+    }
+
+    if (cli.help) {
+        printUsageAndExit()
+    }
+
+    if (cli.version) {
+        printVersionAndExit()
+    }
+
+    if (cli.source == null) {
+        cli.source = Source.MANGA_FOX
+    } else {
+        //init fetcher
+    }
+
+    if (cli.volume != null && cli.chapter != null) {
+        //download chapter
+    } else if (cli.volume != null) {
+        //download volume
+    } else {
+        //download whole manga
+    }
+}
+
+
+fun printUsageAndExit() {
+    jCommander.usage()
+    System.exit(0)
+}
+
+fun printVersionAndExit() {
+    //TODO Fetch version from build.gradle
+    println("Vulpes by Chris Kula v0.1.0")
+    System.exit(0)
+}
