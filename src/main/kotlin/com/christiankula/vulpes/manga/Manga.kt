abfb2616d6d7dcf1f67c635dc10d2d8cafe1ceba
Christian KULA <christian.kula.ck@gmail.com>
2017-08-11 14:52:21 +0200

    Fixed issues with model classes
    Changed Manga's chapters from a Set to a MutableSet (erf)
    Changed Cli's mangaName operand from Nullable String to Non Nullable late init'd String

--- src/main/kotlin/com/christiankula/vulpes/manga/Manga.kt
+++ src/main/kotlin/com/christiankula/vulpes/manga/Manga.kt
@@ -2,4 +2,4 @@ package com.christiankula.vulpes.manga
 
 import java.util.*
 
-data class Manga(val name: String, val url: String, val source: Source, val chapters: Set<Chapter> = TreeSet())
+data class Manga(val name: String, val url: String, val source: Source, val chapters: MutableSet<Chapter> = TreeSet())
