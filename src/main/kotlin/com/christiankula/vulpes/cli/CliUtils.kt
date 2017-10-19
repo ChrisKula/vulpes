96259b43ae449e95fec59732fadc11ee8312f9f0
Christian KULA <christian.kula.ck@gmail.com>
2017-10-19 12:53:29 +0200

    Add a method to print an Error

--- src/main/kotlin/com/christiankula/vulpes/cli/CliUtils.kt
+++ src/main/kotlin/com/christiankula/vulpes/cli/CliUtils.kt
@@ -1,17 +1,22 @@
 package com.christiankula.vulpes.cli
 
 import com.beust.jcommander.JCommander
+import com.christiankula.vulpes.cli.messages.Error
 import com.christiankula.vulpes.log.Log
 
 class CliUtils private constructor() {
 
     companion object {
+        fun printErrorAndExit(error: Error) {
+            printErrorAndExit(error.code, error.message)
+        }
+
         fun printErrorAndExit(exitCode: Int, errorMessage: String) {
             Log.e("$errorMessage (exit code $exitCode)")
             System.exit(exitCode)
         }
 
-        fun printUsageAndExit(jCommander:JCommander) {
+        fun printUsageAndExit(jCommander: JCommander) {
             jCommander.usage()
             System.exit(0)
         }
